c12e9c6c081d8c7096fafe894f016a45d38a07a6
yamam00s <s.yamamo0327@gmail.com>
2021-04-25 22:52:06 +0900

    サンプルの描画 修正

--- components/ThreeDrawing.tsx
+++ components/ThreeDrawing.tsx
@@ -1,40 +1,44 @@
-import React from 'react'
-import * as THREE from 'three'
+import React, { useEffect } from 'react'
+import * as THREE　from 'three'
 
 // https://threejs.org/docs/index.html#manual/ja/introduction/Creating-a-scene
 export const ThreeDrawing = () => {
-  // シーンを作成
-  const scene = new THREE.Scene()
-  // カメラを作成（画角, アスペクト比
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer()
-  // サイズを指定
-  const width = 960
-  const height = 540
-  // サイズを設定
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  const createBox = () => {
+    // シーンを作成
+    const scene = new THREE.Scene()
+    // カメラを作成（画角, アスペクト比
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    // レンダラーを作成
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#render-dom") as HTMLCanvasElement
+    })
+    // サイズを設定
+    renderer.setSize(window.innerWidth, window.innerHeight)
 
-  document.body.appendChild(renderer.domElement)
-  // キューブの作成
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  const cube = new THREE.Mesh(geometry, material)
-  scene.add(cube)
+    document.body.appendChild(renderer.domElement)
+    // キューブの作成
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
 
-  camera.position.z = 5
+    camera.position.z = 5
 
-  // sceneのレンダリング
-  const animate = () => {
-    requestAnimationFrame( animate )
+    // sceneのレンダリング&フレーム処理
+    const animate = () => {
+      requestAnimationFrame(animate)
+      // キューブのアニメーション
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+      renderer.render(scene, camera)
+    }
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
-    renderer.render(scene, camera)
+    animate()
   }
 
-  animate()
+  useEffect(() => {
+    createBox()
+  }, [])
 
-  return <div />
+  return <canvas id="render-dom" />
 }
