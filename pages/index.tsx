eddc5443807831dd29a0b57d11f708f8a0338068
yamam00s <s.yamamo0327@gmail.com>
2021-05-18 09:38:20 +0900

    yarn v2 -> v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
-import React, { useState, lazy, Suspense, useEffect } from "react"
+import React, { useRef, useState, lazy, Suspense, useEffect } from "react"
 const ThreeDrawing = lazy(() => import('components/ThreeDrawing'))
 import styles from 'styles/Home.module.css'
+import { Canvas, useFrame } from '@react-three/fiber'
 
 const Home = () => {
-  const [hasMounted, setHasMounted] = useState(false)
-  useEffect(() => setHasMounted(true), [])
-
   return (
-    hasMounted &&
-      <Suspense fallback={null}>
-        <ThreeDrawing />
-      </Suspense>
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshPhongMaterial />
+        </mesh>
+      </Canvas>
+    </div>
   )
 }
 
